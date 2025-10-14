46d92eaea0ca9f11cac4cb4e91dcb29adc397138
Dylan Nagel <dylan.nagel@carta.com>
2025-10-14 02:09:36 -0400

    Add browser router for clerk routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App.tsx";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { dark } from "@clerk/themes";
+import { BrowserRouter } from "react-router-dom";
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
@@ -14,13 +15,15 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
-      <App />
-    </ClerkProvider>
+    <BrowserRouter basename="Acronymize">
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        appearance={{
+          baseTheme: dark,
+        }}
+      >
+        <App />
+      </ClerkProvider>
+    </BrowserRouter>
   </StrictMode>
 );
