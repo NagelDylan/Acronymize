8acff00f33063564d404d80334bce2056a057bee
Dylan Nagel <dylan.nagel@carta.com>
2025-11-02 16:25:13 -0500

    fixed exit game modal spelling

--- frontend/src/components/game-screen/ExitGameModal.tsx
+++ frontend/src/components/game-screen/ExitGameModal.tsx
@@ -17,7 +17,7 @@ export function ExitGameModal({
       isOpen={isOpen}
       onClose={onClose}
       title="Leaving already?"
-      description="If you quit now, this round's guesses and score will disappear into the void. Want to keep goind?"
+      description="If you quit now, this round's guesses and score will disappear into the void. Want to keep going?"
       primaryBtnDesc="Keep playing"
       secondaryBtnDesc="Exit Anyways"
       onPrimaryClick={onClose}
